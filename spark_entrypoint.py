5275f93dc25974eb5b00a6a37092e589e9230571
Kevin McQuate <kgmcquate@gmail.com>
2023-06-08 18:30:51 -0500

    added jdbc read

--- spark_entrypoint.py
+++ spark_entrypoint.py
@@ -6,8 +6,4 @@ from app.main import main
 
 main()
 
-# url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=
-# temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max,precipitation_sum,rain_sum,showers_sum,snowfall_sum,precipitation_hours,precipitation_probability_max,windspeed_10m_max,windgusts_10m_max,winddirection_10m_dominant,shortwave_radiation_sum,et0_fao_evapotranspiration
-# &forecast_days=1&start_date=2023-05-28&end_date=2023-06-03&timezone=America%2FChicago"
-
 
