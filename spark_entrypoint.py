96251b0fbe288b23efd28e6550a867ef89ff5e84
Kevin McQuate <kgmcquate@gmail.com>
2023-06-08 19:24:56 -0500

    added jdbc read

--- spark_entrypoint.py
+++ spark_entrypoint.py
@@ -1,9 +1,27 @@
 
 from pyspark.sql import SparkSession
 
-from app.main import main
+# from app.main import main
 
 
-main()
+# main()
 
 
+import os
+
+print(os.listdir())
+
+import os 
+files = []
+def print_tree(rootDir):
+    files.append(rootDir)
+    list_dirs = os.walk(rootDir) 
+    for root, dirs, files in list_dirs: 
+        for d in dirs: 
+            files.append(os.path.join(root, d))      
+        for f in files: 
+            files.append(os.path.join(root, f))
+
+print_tree("/home/hadoop/")
+
+[print(f) for f in files]
