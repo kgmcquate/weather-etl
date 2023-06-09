9b4e15d3257bf7fc47f38a94a82de16b3648b01d
Kevin McQuate <kgmcquate@gmail.com>
2023-06-08 22:03:46 -0500

    added iomage to config

--- spark_entrypoint.py
+++ spark_entrypoint.py
@@ -1,30 +1,6 @@
 
 from pyspark.sql import SparkSession
 
-# from app.main import main
+from app.main import main
 
-
-# main()
-
-
-import os
-
-print(os.listdir("/home/hadoop/environment/"))
-print(os.listdir("/home/hadoop/environment/bin/"))
-
-import os 
-
-# def print_tree(rootDir):
-#     files = []
-#     files.append(rootDir)
-#     list_dirs = os.walk(rootDir) 
-#     for root, dirs, files in list_dirs: 
-#         for d in dirs: 
-#             files.append(os.path.join(root, d))      
-#         for f in files: 
-#             files.append(os.path.join(root, f))
-#     return files
-
-# files = print_tree("/home/hadoop/")
-
-# [print(f) for f in files]
+main()
