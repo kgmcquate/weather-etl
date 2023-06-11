4a84f4472ad6b9a614f60a6ca2207c33aeb68834
Kevin McQuate <kgmcquate@gmail.com>
2023-06-11 09:45:50 -0500

    working on api elt

--- src/app/main.py
+++ src/app/main.py
@@ -9,10 +9,17 @@ import pprint
 # import database
 
 api_url = "api.open-meteo.com/v1/"
+DEFAULT_LOOKBACK_DAYS = 365
+lakes_table_name = "lakes"
+weather_by_day_table_name = "weather_by_day"
 
 from .data_models import DailyWeather
 
 features = [feature.name for feature in dataclasses.fields(DailyWeather) if feature.name not in ("date", "latitude", "longitude", "timezone")]
+
+import logging
+
+logger = logging.basicConfig(level=logging.DEBUG)
 
 @dataclass
 class WeatherRequest:
@@ -67,38 +74,119 @@ class WeatherRequest:
             )
 
         return daily_weathers
+
     
 
-def main(spark = SparkSession.builder.getOrCreate()):
+def get_jdbc_options():
     from .database import db_endpoint, db_password, db_username
-
     jdbc_url = f"jdbc:postgresql://{db_endpoint}:5432/"
 
-    print(jdbc_url)
+    logger.debug(jdbc_url)
 
-    df = (
+    return {
+        "dbtable": lakes_table_name,
+        "url": jdbc_url,
+        "user": db_username,
+        "password": db_password,
+        "driver": "org.postgresql.Driver"
+    }
+
+
+def main(
+        spark = SparkSession.builder.getOrCreate(),
+        current_date: datetime.date = datetime.date.today(),
+        lookback_days: int = DEFAULT_LOOKBACK_DAYS,
+
+    ):
+    from pyspark.sql.functions import col, lit, max, min
+    
+    start_date = current_date - datetime.timedelta(days=lookback_days)
+    weather_dates = [current_date - datetime.timedelta(days=x) for x in range(lookback_days) ]
+
+    latlngs_df = (
         spark.read
-        .option("dbtable", "lakes")
-        .option("url", jdbc_url)
-        .option("user", db_username)
-        .option("password", db_password)
-        .option("driver", "org.postgresql.Driver")
+        .option("dbtable", lakes_table_name)
+        .options(**get_jdbc_options())
         .format("jdbc")
         .load()
+        .select("latitude", "longitude")
+        .distinct() # Some locations have the same lat/long
     )
 
-    df.show()
-
-    req = WeatherRequest(
-        start_date=datetime.date.fromisoformat("2023-01-01"),
-        end_date=datetime.date.fromisoformat("2023-01-07"),
-        latitude=52.52,
-        longitude=13.419998
+    weather_days_df = (
+        spark.read
+        .option("dbtable", weather_by_day_table_name)
+        .options(**get_jdbc_options())
+        .format("jdbc")
+        .load()
+        .select("date", "latitude", "longitude")
+        .filter(col("date") >= lit(start_date.isoformat()) )
     )
 
-    pprint.pprint(
-        req.get_weather_data()
+    if logger.isEnabledFor(logging.DEBUG):
+        latlngs_df.show()
+
+    from pyspark.sql.types import StructType, StructField, DateType, StringType, FloatType
+
+    needed_dates_df = (
+        spark.createDataFrame(
+            [[d] for d in weather_dates],
+            schema=StructType().add(field="date", data_type=DateType())
+        )
     )
+
+    if logger.isEnabledFor(logging.DEBUG):
+        latlngs_df.show()
+
+
+    weathers_needed = latlngs_df.crossJoin(needed_dates_df)
+
+    # Weathers that aren't already in the DB
+    weathers_to_get = (
+        weathers_needed
+        .join(weather_days_df, ["latitude", "longitude", "date"], "leftanti")
+    )
+
+    
+    weathers_rdd = (
+        # Get the start and end times for each location, since the API takes a lat/lng and date range
+        weathers_to_get.groupBy("latitude", "longitude").agg(
+            min(col("date")).alias("start_date"),
+            max(col("date")).alias("end_date")
+        )
+        # Turn into API requests
+        .rdd
+        .map(lambda row: WeatherRequest(**row.asDict()))
+        # Only run 5 requests concurrently
+        .coalesce(5)
+        # Send request to API
+        # Each request turns a list of weathers, so flatMap
+        .flatMap(lambda req: req.get_weather_data())
+    )
+
+    # def get_pyspark_type(the_type):
+    #     if the_type == float:
+    #         return FloatType()
+    #     elif the_type == str:
+    #         return StringType()
+
+    # daily_weather_schema = [StructField(name=name, dataType=) for name, python_type in DailyWeather.__annotations__.items()]
+
+    new_weathers = spark.createDataFrame(weathers_rdd)
+
+    new_weathers.show()
+
+
+    # req = WeatherRequest(
+    #     start_date=datetime.date.fromisoformat("2023-01-01"),
+    #     end_date=datetime.date.fromisoformat("2023-01-07"),
+    #     latitude=52.52,
+    #     longitude=13.419998
+    # )
+
+    # pprint.pprint(
+    #     req.get_weather_data()
+    # )
 
 
 
